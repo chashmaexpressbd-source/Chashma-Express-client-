24bc212cb99e13b900ee0c82ff0e06b9f6402775
Md Meherab Hossain Munna <mdmeherab657@gamil.com>
2026-02-03 22:47:58 +0600

    style:design update

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -5,6 +5,8 @@ import ProductCards from './ProductCards';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const getProducts = async () => {
       try {
@@ -14,6 +16,8 @@ const Products = () => {
         setProducts(res.data);
       } catch (error) {
         console.error('Error fetching products:', error);
+      } finally {
+        setLoading(false); // Loading complete
       }
     };
 
@@ -25,9 +29,38 @@ const Products = () => {
       <h2 className="text-2xl font-bold mb-6 text-gray-800">Products</h2>
 
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-6">
-        {products.map(product => (
-          <ProductCards key={product._id} product={product}></ProductCards>
-        ))}
+        {loading ? (
+          <>
+            {' '}
+            <div className="bg-white rounded-md overflow-hidden shadow-md animate-pulse">
+              {/* Image placeholder */}
+              <div className="relative overflow-hidden">
+                <div className="w-full h-40 sm:h-48 md:h-56 bg-gray-200" />
+                {/* Discount badge */}
+                <div className="absolute top-3 left-3 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded">
+                  20% OFF
+                </div>
+              </div>
+
+              {/* Product Info placeholder */}
+              <div className="p-4">
+                <div className="h-5 bg-gray-300 rounded w-3/4 mb-2"></div>
+                <div className="h-4 bg-gray-300 rounded w-full mb-2"></div>
+                <div className="h-4 bg-gray-300 rounded w-5/6 mb-2"></div>
+
+                {/* Price */}
+                <div className="flex items-center space-x-2">
+                  <div className="h-6 w-16 bg-gray-300 rounded"></div>
+                  <div className="h-4 w-12 bg-gray-300 rounded"></div>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          products.map(product => (
+            <ProductCards key={product._id} product={product} />
+          ))
+        )}
       </div>
       {/* Trust Badges */}
       <div className=" md:hidden">
